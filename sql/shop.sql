64c804afffc277f90bb3432f4f136cd800ecc0a6
alex.paclin <alex.paclin@gmail.com>
2019-09-19 17:51:34 +0500

    'sql'

---
+++ sql/shop.sql
@@ -0,0 +1,10 @@
+﻿DROP TABLE IF EXISTS `category`;
+SET character_set_client = utf8;
+CREATE TABLE `category` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+    `category` VARCHAR(255) DEFAULT NULL,
+    `description` TEXT,
+    `image` VARCHAR(500) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
